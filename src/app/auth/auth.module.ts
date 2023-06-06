8bfd18be7012b671a594bb7bf9bd72bf4e903230
Eduardo <analista.sistemas@sedmi.com>
2023-06-05 23:00:39 -0500

    Componentes y pages para formualrios

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 @NgModule({
   declarations: [
     RegisterPageComponent,
-    BasicPageComponent
   ],
   imports: [
     CommonModule,
